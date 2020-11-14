aed0cfc7272c588c9848283cbf198be9cad4295e
Susan-Kathoni <123joey4kathoniey@gmail.com>
2020-11-14 18:11:40 +0300

    Create file that will be responsible for what the user will see.

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http  import HttpResponse
+import datetime as dt
+from gallery.models import Image, Category, Location
+
 
 # Create your views here.
