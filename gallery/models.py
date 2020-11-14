56dd5253802ed4ffb654edb5bff05593ed13c42c
Susan-Kathoni <123joey4kathoniey@gmail.com>
2020-11-14 22:56:13 +0300

    Add image class.

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image = CloudinaryField('image')
+    title = models.CharField(max_length=100)
+    category = models.ForeignKey('Category',default='', on_delete = models.CASCADE)
+    imageDescription = models.CharField(max_length=250)
+    image_location = models.ForeignKey('Location',default='', on_delete = models.CASCADE)
+    date_uploaded = models.DateTimeField(auto_now_add=True, null = True)
+
+    def __str__(self):
+        return self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    
